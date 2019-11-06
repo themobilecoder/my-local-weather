2ec9ef5fc068f2768c5d5e48b1bd43ac4013b972
Rafael Delos Santos <rkdelossantos@gmail.com>
2019-11-06 23:50:26 +1100

    add refresh library and usage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_unit_testing/wave_clipper.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,91 +12,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  RefreshController _controller = RefreshController(initialRefresh: false);
+  void _onRefresh() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    _controller.refreshCompleted();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Stack(
-              children: <Widget>[
-                ClipPath(
-                  clipper: WaveClipper(),
-                  child: Container(
-                    color: Colors.blue,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Text(
-                          'Clear',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 18,
-                              color: Colors.grey.shade50),
-                        ),
-                        SizedBox(height: 10),
-                        Icon(
-                          WeatherIcons.day_sunny,
-                          size: 62,
-                          color: Colors.yellow,
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          '19°',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 72,
-                            color: Colors.white
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 24),
-                    child: FloatingActionButton(
-                      onPressed: () {},
-                      tooltip: 'Add Location',
-                      child: Icon(Icons.add),
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.yellow.shade700,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SmartRefresher(
+        controller: _controller,
+        onRefresh: _onRefresh,
+        enablePullDown: true,
+        header: MaterialClassicHeader(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Stack(
                 children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'SYDNEY, AUSTRALIA',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
+                  ClipPath(
+                    clipper: WaveClipper(),
+                    child: Container(
                       color: Colors.blue,
-                      fontWeight: FontWeight.bold
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Text(
+                            'Clear',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 18,
+                                color: Colors.grey.shade50),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: <Widget>[
+                              Icon(
+                                WeatherIcons.day_sunny,
+                                size: 62,
+                                color: Colors.yellow,
+                              ),
+                              SizedBox(width: 20),
+                              Text(
+                                '19°',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 72,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 24),
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        tooltip: 'Change Location',
+                        child: Icon(Icons.location_city),
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.indigo,
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-        ],
+            Expanded(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'SYDNEY, AUSTRALIA',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          letterSpacing: 1,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
