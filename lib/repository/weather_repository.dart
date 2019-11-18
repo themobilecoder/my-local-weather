c78302ac227a422601e80c13f5c05fe19fbc0f8d
Rafael Delos Santos <rkdelossantos@gmail.com>
2019-11-18 20:09:26 +1100

    Add unit test for weather bloc

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -1,7 +1,7 @@
+import 'package:flutter_unit_testing/bloc/map_coordinate.dart';
 import 'package:flutter_unit_testing/bloc/weather_model.dart';
 
 abstract class WeatherRepository {
   Future<WeatherModel> getWeather();
-  Future<WeatherModel> getWeatherWithLocation(
-      double lattitude, double longitude);
+  Future<WeatherModel> getWeatherWithLocation(MapCoordinate mapCoordinate);
 }
